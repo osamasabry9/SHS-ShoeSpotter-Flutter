b710dd537eb5d6f1320b2f2b90429d03e2e57e64
osamasabry9 <osamasabry324@gmail.com>
2024-04-01 00:04:30 +0200

    Add email verification and success screens to routing
    Introduce routes and screens for email verification and success screens in the
    app router. This allows for navigation to these new screens based on specific
    route paths.

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -2,5 +2,7 @@ class Routes{
   static const String onBoardingScreen = '/onBoardingScreen';
   static const String loginScreen = '/loginScreen';
   static const String signUpScreen = '/signUpScreen';
+  static const String emailVerificationScreen = '/emailVerificationScreen';
+  static const String successScreen = '/successScreen';
   static const String homeScreen = '/homeScreen';
 }
