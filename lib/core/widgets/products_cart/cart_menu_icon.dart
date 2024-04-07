0d0d3f68694aac346d325a097c547fefebe1ae0c
osamasabry9 <osamasabry324@gmail.com>
2024-04-08 01:52:04 +0200

    Add CartMenuIcon widget
    Implement CartMenuIcon widget with IconButton and Badge to display a shopping bag icon with a badge indicating the number of items in the cart.

---
+++ lib/core/widgets/products_cart/cart_menu_icon.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import '../../utils/constants/colors.dart';
+
+class CartMenuIcon extends StatelessWidget {
+  final Color iconColor;
+  final VoidCallback onPressed;
+  const CartMenuIcon({
+    super.key,
+    required this.iconColor,
+    required this.onPressed,
+  });
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: onPressed,
+      icon: Badge(
+        label: Text(
+          '2',
+          style: Theme.of(context)
+              .textTheme
+              .labelLarge!
+              .apply(color: AppColors.white, fontSizeFactor: 0.8),
+        ),
+        backgroundColor: AppColors.black,
+        child:  Icon(
+          Iconsax.shopping_bag,
+          color: iconColor,
+        ),
+      ),
+    );
+  }
+}
