bc2b996b89837881abaddf84712665b69dfa7d2b
osamasabry9 <osamasabry324@gmail.com>
2024-04-29 13:45:35 +0300

    Refactor cart widgets for better flexibility
    Add conditional rendering for add/remove buttons in cart list items
    and checkout button. Improve code readability and maintainability.

--- lib/features/shop/presentation/pages/cart/widgets/list_view_cart_widget.dart
+++ lib/features/shop/presentation/pages/cart/widgets/list_view_cart_widget.dart
@@ -1,22 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:shoe_spotter/core/utils/constants/sizes.dart';
-import 'package:shoe_spotter/features/shop/presentation/pages/cart/widgets/list_item_cart_widget.dart';
+import '../../../../../../core/utils/constants/sizes.dart';
+import 'list_item_cart_widget.dart';
 
 class ListViewCartWidget extends StatelessWidget {
   const ListViewCartWidget({
     super.key,
+    this.showAddRemoveButtons = true,
   });
-
+  final bool showAddRemoveButtons;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(AppSizes.defaultSpace),
       child: ListView.separated(
         shrinkWrap: true,
-        itemCount: 14,
+        physics:
+            showAddRemoveButtons ? null : const NeverScrollableScrollPhysics(),
+        itemCount: 3,
         separatorBuilder: (_, __) =>
             const SizedBox(height: AppSizes.spaceBtwSections),
-        itemBuilder: (_, index) => const ListItemCartWidget(),
+        itemBuilder: (_, index) =>
+            ListItemCartWidget(showAddRemoveButtons: showAddRemoveButtons),
       ),
     );
   }
