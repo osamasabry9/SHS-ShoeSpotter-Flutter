410032573d09d3de0ef19b2e8bcfe9abca84465e
osamasabry9 <osamasabry324@gmail.com>
2024-04-29 17:01:37 +0300

    Add order screen and related widgets for displaying order details
    This commit adds the OrderScreen along with OrderItemWidget, OrderListItemsWidget,
    OrderNumberWidget, ProcessingSectionWidget, and ShippingDateWidget to display order
    details in the shopping app.

---
+++ lib/features/shop/presentation/pages/order/widgets/order_number_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import '../../../../../../core/utils/constants/sizes.dart';
+
+class OrderNumberWidget extends StatelessWidget {
+  const OrderNumberWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        children: [
+          /// 1 - Icon
+          const Icon(Iconsax.tag),
+          const SizedBox(width: AppSizes.spaceBtwItems / 2),
+    
+          /// 2 - status and date
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Order",
+                    style: Theme.of(context).textTheme.labelMedium),
+                Text("[#256f2]",
+                    style: Theme.of(context).textTheme.headlineSmall),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
