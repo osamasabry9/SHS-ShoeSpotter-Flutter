70a39e190838acc3c46f57868892b356d4c026d0
osamasabry9 <osamasabry324@gmail.com>
2024-05-09 04:28:11 +0300

    Initialize Firebase and remove unnecessary code
    Removed redundant Firebase initialization code and updated the Flutter binding
    to ensure it is properly initialized before running the app.

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -3,20 +3,19 @@ import 'package:flutter/material.dart';
 
 import 'app/di.dart';
 import 'app/t_shoe_spotter_app.dart';
-import 'firebase_options.dart';
 
 Future<void> main() async {
   // Widgets binding
-  //final WidgetsBinding binding =
-   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp();
+
 // Initialize GetIt
-  await initAppModule();
+ // await initAppModule();
+
   // await Splash until other items are loaded
   // FlutterNativeSplash.preserve(widgetsBinding: binding);
 
-  // Initialize Firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(TShoeSpotterApp());
 }
