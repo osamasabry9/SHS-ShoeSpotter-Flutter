6406d78450684f4ab4c44d01c8c0435f8ca73482
osamasabry9 <osamasabry324@gmail.com>
2024-05-11 15:55:51 +0300

    Add OnBoardingController and update widgets to use controller
    The commit adds the OnBoardingController class to manage the onboarding
    screen logic and updates the widgets to interact with the controller for
    navigation and state management. This improves code organization and
    maintainability.

---
+++ lib/features/onboarding/controller/onboarding_controller.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import '../../../core/routing/routes.dart';
+
+import '../../../app/di.dart';
+import '../../../core/utils/constants/text_strings.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  /// Variables
+  RxInt currentIndex = 0.obs;
+  final pageController = PageController();
+
+  @override
+  void onInit() {
+    getIt<GetStorage>()
+        .write(AppTexts.PREFS_KEY_ONBOARDING_SCREEN_VIEWED, true);
+    super.onInit();
+  }
+
+  // Updates the current index
+  void updateIndex(int index) => currentIndex.value = index;
+
+  // Jump to the specified dot selected page.
+  void dotNavigationClick(index) {
+    currentIndex.value = index;
+    pageController.jumpToPage(index);
+  }
+
+  // Updates the current index and jump to the selected page.
+  void nextPage() {
+    if (currentIndex.value == 2) {
+      Get.offAllNamed(Routes.loginScreen);
+    } else {
+      int page = currentIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
+
+  void skipPage() {
+    Get.offAllNamed(Routes.loginScreen);
+  }
+}
--- lib/features/onboarding/presentation/pages/onboarding_screen.dart
+++ lib/features/onboarding/presentation/pages/onboarding_screen.dart
@@ -1,34 +1,19 @@
 import 'package:flutter/material.dart';
-import '../../../../core/local_storage/storage_utility.dart';
-import '../../../../core/utils/constants/text_strings.dart';
+import 'package:get/get.dart';
 
-import '../../../../app/di.dart';
 import '../../../../core/utils/constants/sizes.dart';
+import '../../controller/onboarding_controller.dart';
 import '../widgets/on_boarding_next_button_widget.dart';
 import '../widgets/on_boarding_page_view_items.dart';
 import '../widgets/on_boarding_skip_widget.dart';
 import '../widgets/smooth_page_indicator_widget.dart';
 
-class OnBoardingScreen extends StatefulWidget {
+class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
-  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
-}
-
-class _OnBoardingScreenState extends State<OnBoardingScreen> {
-  final PageController onboardController = PageController();
-  bool isLast = false;
-  final AppLocalStorage _appLocalStorage = instance<AppLocalStorage>();
-  @override
-  void initState() {
-    _appLocalStorage.saveData(
-        AppTexts.PREFS_KEY_ONBOARDING_SCREEN_VIEWED, true);
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final controller = Get.put(OnBoardingController());
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(AppSizes.defaultSpace),
@@ -38,35 +23,20 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
             const OnBoardingSkipWidget(),
             Expanded(
               child: PageView.builder(
-                controller: onboardController,
+                controller: controller.pageController,
                 physics: const BouncingScrollPhysics(),
                 itemBuilder: (context, index) =>
                     OnBoardingPageViewItems.introPages[index],
-                onPageChanged: (index) {
-                  if (index == OnBoardingPageViewItems.introPages.length - 1) {
-                    setState(() {
-                      isLast = true;
-                    });
-                  } else {
-                    setState(() {
-                      isLast = false;
-                    });
-                  }
-                },
+                onPageChanged: (index) => controller.updateIndex(index),
                 itemCount: OnBoardingPageViewItems.introPages.length,
               ),
             ),
             const SizedBox(height: 10),
-            Row(
+            const Row(
               children: [
-                SmoothPageIndicatorWidget(
-                  onboardController: onboardController,
-                ),
-                const Spacer(),
-                OnBoardingNextButtonWidget(
-                  isLast: isLast,
-                  onboardController: onboardController,
-                )
+                SmoothPageIndicatorWidget(),
+                Spacer(),
+                OnBoardingNextButtonWidget()
               ],
             ),
           ],
