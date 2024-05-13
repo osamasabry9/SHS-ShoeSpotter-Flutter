5fd35ac35abba84c5863ae49635c1a62aeea8257
osamasabry9 <osamasabry324@gmail.com>
2024-05-13 18:21:33 +0300

    Add new constants for shared preferences keys
    Added new constants for shared preferences keys in the `AppPrefsKeys` class
    to improve code organization and readability. Removed duplicate constants
    from the `AppTexts` class.

--- lib/core/utils/constants/api_constants.dart
+++ lib/core/utils/constants/api_constants.dart
@@ -1,6 +1,18 @@
-
 // ignore_for_file: constant_identifier_names
 
-class FirebaseConst{
+class FirebaseConst {
   static const String USERS_COLLECTION = "USERS_COLLECTION";
+}
+
+class AppPrefsKeys {
+  // Keys
+  static const String PREFS_KEY_LANG = "PREFS_KEY_LANG";
+  static const String PREFS_KEY_ONBOARDING_SCREEN_VIEWED =
+      "PREFS_KEY_ONBOARDING_SCREEN_VIEWED";
+  static const String PREFS_KEY_IS_USER_LOGGED_IN =
+      "PREFS_KEY_IS_USER_LOGGED_IN";
+  static const String PREFS_KEY_REMEMBER_ME_EMAIL =
+      "PREFS_KEY_REMEMBER_ME_EMAIL";
+  static const String PREFS_KEY_REMEMBER_ME_PASSWORD =
+      "PREFS_KEY_REMEMBER_ME_PASSWORD";
 }
