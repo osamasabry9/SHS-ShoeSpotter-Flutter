e69f98133f14887a0af921ee1cbbb34f7fef2726
osamasabry9 <osamasabry324@gmail.com>
2024-05-13 18:21:42 +0300

    Add full screen loader and snack bar loaders
    Added a full screen loader for displaying loading dialogs and snack bar loaders
    for displaying warning, success, and error messages in the application.

---
+++ lib/core/utils/popups/loaders.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+import '../constants/colors.dart';
+
+class AppLoaders {
+  static void warningSnackBar({required String title, message = ""}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: Colors.white,
+      backgroundColor: Colors.orange,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.all(20),
+      icon: const Icon(Iconsax.warning_2, color: Colors.white),
+    );
+  }
+
+  static void successSnackBar({required String title, message = ""}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: Colors.white,
+      backgroundColor: AppColors.primary,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.all(20),
+      icon: const Icon(Iconsax.check, color: Colors.white),
+    );
+  }
+
+  static void errorSnackBar({required String title, message = ""}) {
+    Get.snackbar(
+      title,
+      message,
+      isDismissible: true,
+      shouldIconPulse: true,
+      colorText: Colors.white,
+      backgroundColor: Colors.red.shade600,
+      snackPosition: SnackPosition.BOTTOM,
+      duration: const Duration(seconds: 3),
+      margin: const EdgeInsets.all(20),
+      icon: const Icon(Iconsax.warning_2, color: Colors.white),
+    );
+  }
+}
