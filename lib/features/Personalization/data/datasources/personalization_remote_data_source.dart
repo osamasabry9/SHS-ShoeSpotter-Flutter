a0a02e2920d52ebdb0782515056efd30b86f25b8
osamasabry9 <osamasabry324@gmail.com>
2024-05-23 06:16:24 +0300

    feat: Add new methods to PersonalizationRemoteDataSource and PersonalizationRepositoryImpl
    This commit adds new methods to the PersonalizationRemoteDataSource and PersonalizationRepositoryImpl classes. The new methods include fetchUserDetails, updateUserDetails, updateSingleField, uploadImage, and reAuthenticateEmailAndPassword. These methods allow for more functionality in the personalization feature, such as fetching user details, updating user details, uploading images, and re-authenticating email and password. The commit also includes some minor code refactoring.

--- lib/features/Personalization/data/datasources/personalization_remote_data_source.dart
+++ lib/features/Personalization/data/datasources/personalization_remote_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../auth/data/models/user_model.dart';
 
@@ -6,6 +7,14 @@ abstract class PersonalizationRemoteDataSource {
   Future<void> saveUserRecordInFirestore(UserModel user);
 
   Future<void> saveUserData(UserCredential? userCredential);
+
+  Future<UserModel> fetchUserDetails();
+
+  Future<void> updateUserDetails(UserModel user);
+  Future<void> updateSingleField(Map<String, dynamic> jsonField);
+   Future<String> uploadImage(String path, XFile image);
+
+  Future<void> reAuthenticateEmailAndPassword(String email, String password );
 
   Future<void> logoutUser();
 
