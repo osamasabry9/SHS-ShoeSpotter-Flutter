7fafcbcb8dfd7bbd1c1b1194c9e065e640de190f
osamasabry9 <osamasabry324@gmail.com>
2024-05-28 17:46:37 +0300

    Refactor home screen and banner controller
    - Refactored the home screen by removing unused imports and simplifying the layout.
    - Refactored the banner controller to use dependency injection and added a method to upload banners to the cloud firestore. Also updated the PromoSlider widget to use the new banner controller and added loading and error handling.

--- lib/features/shop/presentation/pages/home/widgets/promo_slider.dart
+++ lib/features/shop/presentation/pages/home/widgets/promo_slider.dart
@@ -5,46 +5,66 @@ import 'package:get/get.dart';
 import '../../../../../../core/utils/constants/colors.dart';
 import '../../../../../../core/utils/constants/sizes.dart';
 import '../../../../../../core/widgets/custom_shapes/containers/circular_container_widget.dart';
+import '../../../../../../core/widgets/custom_shapes/shimmer/app_shimmer_effect.dart';
 import '../../../../../../core/widgets/images/rounded_image_widget.dart';
-import '../../../controllers/home_controller.dart';
+import '../../../controllers/banner_controller.dart';
 
 class PromoSlider extends StatelessWidget {
-  final List<String> banners;
   const PromoSlider({
     super.key,
-    required this.banners,
   });
 
   @override
   Widget build(BuildContext context) {
-    final homeController = Get.put(HomeController());
-    return Column(
-      children: [
-        CarouselSlider(
-          items: banners
-              .map((e) => RoundedImageWidget(imageUrl: e))
-              .toList(),
-          options: CarouselOptions(
-              viewportFraction: 1,
-              onPageChanged: (index, _) =>
-                  homeController.updatePageIndicator(index)),
-        ),
-        const SizedBox(height: AppSizes.spaceBtwItems),
-        Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            for (int i = 0; i < banners.length; i++)
-              CircularContainerWidget(
-                width: 20,
-                height: 4,
-                backgroundColor: homeController.carousalCurrentIndex.value == i
-                    ? AppColors.primary
-                    : AppColors.grey,
-                margin: const EdgeInsets.only(right: 10),
+    final bannerController = Get.put(BannerController());
+    return Obx(() {
+      if (bannerController.isLoading.value) {
+        return const AppShimmerEffectWidget(
+            width: double.infinity, height: 190, radius: 10);
+      }
+      if (bannerController.allBanners.isEmpty) {
+        return const Center(
+          child:
+              Text("No Data found!", style: TextStyle(color: AppColors.white)),
+        );
+      }
+      return Column(
+        children: [
+          CarouselSlider(
+            items: bannerController.allBanners
+                .map((banner) => RoundedImageWidget(
+                      imageUrl: banner.imageUrl,
+                      isNetworkImage: true,
+                      onPressed: () => Get.toNamed(banner.targetScreen),
+                    ))
+                .toList(),
+            options: CarouselOptions(
+                viewportFraction: 1,
+                onPageChanged: (index, _) =>
+                    bannerController.updatePageIndicator(index)),
+          ),
+          const SizedBox(height: AppSizes.spaceBtwItems),
+          Center(
+            child: Obx(
+              () => Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  for (int i = 0; i < bannerController.allBanners.length; i++)
+                    CircularContainerWidget(
+                      width: 20,
+                      height: 4,
+                      backgroundColor:
+                          bannerController.carousalCurrentIndex.value == i
+                              ? AppColors.primary
+                              : AppColors.grey,
+                      margin: const EdgeInsets.only(right: 10),
+                    ),
+                ],
               ),
-          ],
-        )
-      ],
-    );
+            ),
+          )
+        ],
+      );
+    });
   }
 }
