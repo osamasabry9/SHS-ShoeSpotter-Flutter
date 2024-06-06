b91dd28600a7a0ba75b7135f2d123049105c7e56
osamasabry9 <osamasabry324@gmail.com>
2024-06-06 06:51:41 +0300

    Add dynamic product fetching to AllProductsScreen
    Enhanced AllProductsScreen to support dynamic fetching of products
    using Firestore queries and future methods. Introduced a new
    AllProductsController to manage product fetching logic. Updated
    HomeScreen to navigate to AllProductsScreen with appropriate
    query parameters for fetching featured products. Added necessary
    imports and refactored the UI to handle loading states and display
    products accordingly.

--- lib/features/shop/presentation/pages/home/home_screen.dart
+++ lib/features/shop/presentation/pages/home/home_screen.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../../../core/utils/constants/api_constants.dart';
 
-import '../../../../../core/routing/routes.dart';
+import '../../../../../app/di.dart' as di;
+
 import '../../../../../core/utils/constants/sizes.dart';
-import '../../../../../core/utils/helpers/extensions.dart';
 import '../../../../../core/widgets/custom_shapes/containers/custom_search_container.dart';
 import '../../../../../core/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../../core/widgets/custom_shapes/shimmer/vertical_product_shimmer_widget.dart';
@@ -11,6 +13,7 @@ import '../../../../../core/widgets/layouts/grid_layout_widget.dart';
 import '../../../../../core/widgets/products/products_card/product_card_vertical_widget.dart';
 import '../../../../../core/widgets/texts/section_heading.dart';
 import '../../controllers/product/product_controller.dart';
+import '../all_products/all_products_screen.dart';
 import 'widgets/home_app_bar.dart';
 import 'widgets/home_categories_section.dart';
 import 'widgets/promo_slider.dart';
@@ -54,8 +57,16 @@ class HomeScreen extends StatelessWidget {
                   /// Heading
                   SectionHeading(
                       title: "Popular Products",
-                      onPressed: () =>
-                          context.pushNamed(Routes.allProductsScreen)),
+                      onPressed: () => Get.to(() => AllProductsScreen(
+                            title: "Popular Products",
+                            query: di
+                                .getIt<FirebaseFirestore>()
+                                .collection(FirebaseConst.PRODUCTS_COLLECTION)
+                                .where('isFeatured', isEqualTo: true)
+                                .limit(6),
+                            futureMethod:
+                                productController.fetchAllFeaturedProducts(),
+                          ))),
                   const SizedBox(
                     height: AppSizes.spaceBtwItems,
                   ),
