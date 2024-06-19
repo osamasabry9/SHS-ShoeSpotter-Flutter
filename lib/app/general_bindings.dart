5ddb5779a82f23e1280395afee3eca2d8d6a0b8c
osamasabry9 <osamasabry324@gmail.com>
2024-06-19 21:18:10 +0300

    Add VariationController to GeneralBindings
    Import VariationController and add it to the dependencies in GeneralBindings.

--- lib/app/general_bindings.dart
+++ lib/app/general_bindings.dart
@@ -1,11 +1,14 @@
 import 'package:get/get.dart';
 
 import '../core/utils/helpers/network_manager.dart';
+import '../features/shop/presentation/controllers/product/variation_controller.dart';
 
 class GeneralBindings implements Bindings {
   @override
   void dependencies() {
     Get.put(NetworkManager());
+
+    Get.put(VariationController());
 
   }
 }
