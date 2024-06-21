c036c84ad0694f7f8f132d8fbb33d7586cf24a14
osamasabry9 <osamasabry324@gmail.com>
2024-06-21 19:59:27 +0300

    Add ApiService and AppConstants classes for networking
    Add ApiService class with post method for making API requests and AppConstants
    class with base URLs for networking operations.

---
+++ lib/core/networking/app_constants.dart
@@ -0,0 +1,5 @@
+class AppConstants {
+  static const String baseUrl = 'https://api.stripe.com/v1';
+  static const String paymentIntentsUrl = '$baseUrl/payment_intents';
+  static const String ephemeralKeyUrl = "$baseUrl/ephemeral_keys";
+}
